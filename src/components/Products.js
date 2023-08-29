bb8a838bdbd9e6f280dae3283fffb20ab3e3bc66
himanshusk911 <himanshusk911@gmail.com>
2023-08-29 13:23:52 +0530

    removed discripancy

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,25 +4,34 @@ import { addToCart } from '../Redux/action'
 import './index.css'
 const Products = () => {
   const products=useSelector((state)=>state.allProducts.products)
-  console.log(products)
+  console.log(products.length)
   const dispatch=useDispatch()
   const handleAddToCart=(product)=>{
     dispatch(addToCart(product))
     console.log(addToCart(product))
   }
+  const noData=()=>{
+    <div>
+      <h2>No Data</h2>
+    </div>
+  }
   const renderList=products.map((product)=>{
-    const {id,title,images,price,category}=product
-    return(
-      <div className='card'>
-      <div className='col-md-3 mt-4' key={id}>
-         <img src={images[0]}  alt={title}/>
-        <p className='font-weight-bold '>{title}</p>
-        <p className='font-weight-bold text-center'>{category.name}</p>
-        <h5 className='font-weight-bold text-center'>${price}</h5>
-        <button onClick={()=>handleAddToCart(product)}>Add To Cart</button>
-      </div>
-      </div>
-    )
+    
+   
+      const {id,title,images,price,category}=product
+      return(
+        <div className='card'>
+        <div className='col-md-3 mt-4' key={id}>
+           <img src={images[0]}  alt={title}/>
+          <p className='font-weight-bold '>{title}</p>
+          <p className='font-weight-bold text-center'>{category.name}</p>
+          <h5 className='font-weight-bold text-center'>${price}</h5>
+          <button onClick={()=>handleAddToCart(product)}>Add To Cart</button>
+        </div>
+        </div>
+      )
+    
+   
   })
   return (
     <div className='container'>
