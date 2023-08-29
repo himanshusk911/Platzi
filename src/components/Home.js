c87239a71e7a6eddf846d7304806ddd195c8bb3c
himanshusk911 <himanshusk911@gmail.com>
2023-08-29 13:23:21 +0530

    added nodata component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,22 @@ import { useDispatch,useSelector } from 'react-redux'
 import { setProducts } from '../Redux/action'
 import Products from './Products'
 import'./index.css'
+import Nodata from './Nodata'
 const Home = () => {
   const dispatch = useDispatch();
   const searchText=useSelector(state=>state.search.searchText)
- 
+ const products=useSelector(state=>state.allProducts.products)
+ console.log(products.length)
   useEffect(()=>{
     const fetchProducts=async()=>{
       try{
         const response=await axios
         .get(`https://dummyjson.com/products/search?q=${searchText}`)
         console.log(response.data)
-        dispatch(setProducts(response.data.products));
+       
+          dispatch(setProducts(response.data.products));
+        
+       
       }catch (error){
         console.log("Error",error)
 }
@@ -25,7 +30,8 @@ const Home = () => {
   
   return(
     <div >
-      <Products/>
+      {products.length >0 ?<Products/>:<Nodata/>}
+      
      
     </div>
   )
